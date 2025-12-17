5e101ee6e41e5601b827e4584d4c60b751fdc9b6
Wanda Feng <wanda@stust.edu.tw>
2025-12-17 11:50:18 +0800

    upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/group6/',
+      base: '/group6-main/',
       server: {
         port: 3000,
         host: '0.0.0.0',
